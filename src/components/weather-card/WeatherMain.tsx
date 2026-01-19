24611b123e27adb3cba9fb493c019adde56fe75c
madaminovsaidaxmad <parolvenom111@gmail.com>
2026-01-19 10:03:20 +0500

    fix weather main

--- src/components/weather-card/WeatherMain.tsx
+++ src/components/weather-card/WeatherMain.tsx
@@ -1,49 +1,60 @@
 import { useWeather } from "../../hooks/useWeather";
+import type { Units } from "../Layout/Layout";
 
-function WeatherMain({ city }: { city: string }) {
-    const { data, isLoading } = useWeather(city);
+interface Props {
+  city: string;
+  units: Units;
+}
 
-    if (isLoading || !data) return <p className='text-white'>Loading...</p>;
+function WeatherMain({ city, units }: Props) {
+  const { data, isLoading } = useWeather(city);
 
-    const date = new Date(data.location.localtime);
-    const formattedDate = date.toLocaleDateString("en-US", {
-        weekday: "long",
-        month: "short",
-        day: "numeric",
-        year: "numeric",
-    });
+  if (isLoading || !data) return <p className="text-white">Loading...</p>;
 
-    return (
-        <section>
-            <div className='container'>
-                <div className='weather-card md:flex md:justify-between md:items-center'>
-                    <div className='weather-main-texts'>
-                        <h4 className='weather-main-title text-white text-2xl font-semibold'>
-                            {data.location.name}, {data.location.country}
-                        </h4>
+  const date = new Date(data.location.localtime);
+  const formattedDate = date.toLocaleDateString("en-US", {
+    weekday: "long",
+    month: "short",
+    day: "numeric",
+    year: "numeric",
+  });
 
-                        <p className='weather-main-subtitle text-white/70'>
-                            {formattedDate}
-                        </p>
-                    </div>
+  const temp =
+    units.temp === "c"
+      ? data.current.temp_c
+      : (data.current.temp_c * 9) / 5 + 32;
 
-                    <div className='weather-main-info flex items-center gap-8'>
-                        <div className='w-30 h-30'>
-                            <img
-                                className='w-full h-full'
-                                src={data.current.condition?.icon}
-                                alt={data.current.condition?.text}
-                            />
-                        </div>
+  return (
+    <section>
+      <div className="container">
+        <div className="weather-card md:flex md:justify-between md:items-center">
+          <div className="weather-main-texts">
+            <h4 className="weather-main-title text-white text-2xl font-semibold">
+              {data.location.name}, {data.location.country}
+            </h4>
 
-                        <h2 className='text-6xl text-white'>
-                            {Math.round(data.current.temp_c)}°
-                        </h2>
-                    </div>
-                </div>
+            <p className="weather-main-subtitle text-white/70">
+              {formattedDate}
+            </p>
+          </div>
+
+          <div className="weather-main-info flex items-center gap-8">
+            <div className="w-30 h-30">
+              <img
+                className="w-full h-full"
+                src={data.current.condition.icon}
+                alt={data.current.condition.text}
+              />
             </div>
-        </section>
-    );
+
+            <h2 className="text-6xl text-white">
+              {Math.round(temp)}°{units.temp.toUpperCase()}
+            </h2>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default WeatherMain;
